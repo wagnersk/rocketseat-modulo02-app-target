76ac13524b123fd0befacd3c2e827a393b2f87c9
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-28 00:01:06 -0300

    Assets da aplicação

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import {colors } from "@/theme/colors"
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ola</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
