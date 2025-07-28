a39d4ecd111b206e8bf0cf1a756e769a5d8e97d0
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-28 00:37:57 -0300

    Adicionando icone no app

--- android/app/src/main/java/com/wagner/target/MainActivity.kt
+++ android/app/src/main/java/com/wagner/target/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.wagner.target
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/wagner/target/MainApplication.kt
+++ android/app/src/main/java/com/wagner/target/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.wagner.target
 
 import android.app.Application
 import android.content.res.Configuration
