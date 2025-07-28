bbcb4cddd2575b45bcc82a9ecd3a7c1c5cd5e6e9
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-28 00:52:21 -0300

    Navegação baseada em arquivos

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View ,Text} from "react-native";
+
+export default function Index(){
+    return (
+        <View style={{flex:1,justifyContent:'center'}}>
+            <Text>Expo Router</Text>
+        </View>
+    )
+}
