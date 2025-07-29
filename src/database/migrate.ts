394012c21c12c197fd99362d2899c1a01c421dd9
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-29 16:23:19 -0300

    SQLite - Criando a tabela Transactions

---
+++ src/database/migrate.ts
@@ -0,0 +1,17 @@
+import { type SQLiteDatabase } from 'expo-sqlite'
+
+export async function migrate(database: SQLiteDatabase) {
+  return database.execAsync(`
+      PRAGMA foreign_key = ON;
+
+      CREATE TABLE IF NOT EXISTS targets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        amount FLOAT NOT NULL,
+
+        created_at timestamp NOT NULL DEFAULT current_timestamp,
+        updated_at timestamp NOT NULL DEFAULT current_timestamp
+      );
+
+    `)
+}
