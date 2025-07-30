6d316d5f631185e20de8c0189be54fb1f7a0b40d
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-30 01:06:16 -0300

    SQLite - Calculando transações

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -6,22 +6,21 @@ export type TargetCreate = {
 }
 
 export type TargetResponse = {
-  id:number
+  id: number
   name: string
   amount: number
   current: number
-  percentage:number
+  percentage: number
   created_at: Date
   updated_at: Date
 }
 
 export function useTargetDatabase() {
-    
   const database = useSQLiteContext()
 
   async function create(data: TargetCreate) {
     const statement = await database.prepareAsync(
-      "INSERT INTO targets (name, amount) VALUES ($name, $amount)",
+      'INSERT INTO targets (name, amount) values ($name, $amount)',
     )
 
     statement.executeAsync({
@@ -30,20 +29,20 @@ export function useTargetDatabase() {
     })
   }
 
-  function listBySavedValue(){
+  function listBySavedValue() {
     return database.getAllAsync<TargetResponse>(`
-       SELECT
-        targets.id,
-        targets.name,
-        targets.amount
-       FROM targets
-
+        SELECT
+          targets.id,
+          targets.name,
+          targets.amount,
+          COALESCE (SUM(transactions.amount), 0) AS current,
+          COALESCE ((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage
+        FROM targets
+        LEFT JOIN transactions ON targets.id = transactions.target_id
+        GROUP BY targets.id, targets.name, targets.amount
+        ORDER BY current DESC
       `)
-
   }
 
-  return { 
-    create,
-    listBySavedValue,
-   }
+  return { create, listBySavedValue }
 }
