256cacf681677f43cb00bd39d5a652723db4256a
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-07-30 15:37:17 -0300

    SQLite - Método de remover transação

--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -1,52 +1,50 @@
-import { useSQLiteContext } from "expo-sqlite";
+import { useSQLiteContext } from 'expo-sqlite'
 
-export type TransactionsCreate = {
-    target_id:number
-    amount:number
-    observation?:string
+export type TransactionCreate = {
+  target_id: number
+  amount: number
+  observation?: string
 }
 
 export type TransactionResponse = {
-    id:number
-    target_id:number
-    amount:number
-    observation:string
-    created_at:Date
-    updated_at:Date
+  id: number
+  target_id: number
+  amount: number
+  observation: string
+  created_at: Date
+  updated_at: Date
 }
 
 export function useTransactionsDatabase() {
-    const database = useSQLiteContext()
+  const database = useSQLiteContext()
 
-    async function create(data:TransactionsCreate) {
-        const statement = await database.prepareAsync(`
-            INSERT INTO  transactions
-                (target_id, amount, observation)
-            VALUES
-                ($target_id, $amount, $observation)
-            `
-        )
+  async function create(data: TransactionCreate) {
+    const statement = await database.prepareAsync(`
+        INSERT INTO transactions
+          (target_id, amount, observation)
+        VALUES
+          ($target_id, $amount, $observation)
+      `)
 
-        statement.executeAsync({
-            $target_id:data.target_id,
-            $amount:data.amount,
-            $observation:data.observation,
-        })
-    }
+    statement.executeAsync({
+      $target_id: data.target_id,
+      $amount: data.amount,
+      $observation: data.observation,
+    })
+  }
 
-    function listByTargetId(id:number) {
-        return database.getAllAsync<TransactionResponse>(`
-            SELECT id, target_id, amount, observation, created_at, updated_at
-            FROM transactions
-            WHERE target_id = ${id}
-            ORDER BY created_at DESC
-            `)
-    }
+  function listByTargetId(id: number) {
+    return database.getAllAsync<TransactionResponse>(`
+        SELECT id, target_id, amount, observation, created_at, updated_at
+        FROM transactions
+        WHERE target_id = ${id}
+        ORDER BY created_at DESC
+      `)
+  }
 
-    return {
-        create,
-        listByTargetId,
+  async function remove(id: number) {
+    await database.runAsync('DELETE FROM transactions WHERE id = ?', id)
+  }
 
-    }
-
+  return { create, listByTargetId, remove }
 }
